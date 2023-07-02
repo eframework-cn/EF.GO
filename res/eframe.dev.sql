73b52ceb59b062e770ad96f109719380c12f9060
wellshsu <wellshsu@outlook.com>
2023-07-02 16:31:46 +0800

    1.update db config

---
+++ res/eframe.dev.sql
@@ -0,0 +1,39 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50520
+ Source Host           : localhost:3306
+ Source Schema         : eframe.dev
+
+ Target Server Type    : MySQL
+ Target Server Version : 50520
+ File Encoding         : 65001
+
+ Date: 02/07/2023 16:24:20
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for player
+-- ----------------------------
+DROP TABLE IF EXISTS `player`;
+CREATE TABLE `player`  (
+  `id` int(11) NOT NULL,
+  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `online` int(11) UNSIGNED ZEROFILL NOT NULL,
+  `conn_url` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `conn_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of player
+-- ----------------------------
+INSERT INTO `player` VALUES (0, 'test01', '123456', 00000000000, '', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
