5701d9b406cd2a1c89350f718d4cfb60cc9217cc
wellshsu <wellshsu@outlook.com>
2023-07-12 11:57:29 +0800

    1.update jenkins scripts 2.upgrade lib

--- .jenkins/libs/Start.ts
+++ .jenkins/libs/Start.ts
@@ -9,7 +9,6 @@
 //-----------------------------------------------------------------------//
 
 import * as path from "path"
-import * as http from "http"
 import * as child_process from "child_process"
 import { Helper } from "./Helper"
 import { Const, Scheme } from "./Const"
@@ -18,80 +17,74 @@ export class Start {
     public static Process(svrs?: string[]): Promise<string> {
         return new Promise<string>(async (resolve, reject) => {
             try {
-                let lcfg: any
-                let sf = path.join(Const.WORKSAPCE, "exec/public/conn/conf/server.json")
-                if (Helper.HasFile(sf)) lcfg = require(sf)
-                else {
-                    sf = path.join(Const.WORKSAPCE, "exec/public/cgi/conf/server.json")
-                    if (Helper.HasFile(sf)) lcfg = require(sf)
-                }
-                if (!lcfg) {
-                    let err = "no default config(conn/cgi) was found."
-                    Helper.LogError("[Start]err: {0}", err)
-                    reject(err)
-                } else {
-                    let plat = Helper.GetPlat()
-                    let arch = Helper.GetArch()
-                    let root = Const.WORKSAPCE
-                    let settings = require(path.join(root, ".vscode/settings.json"))
-                    let rawList = settings["ecode-go.targetList"]
-                    let targets = new Map<string, Scheme>()
-                    for (let name in rawList) {
-                        let otarget = rawList[name]
-                        let temp = {}
-                        for (let key in otarget) {
-                            let raw = otarget[key]
-                            let base = temp[raw.base]
-                            let scheme = new Scheme(name, key, base, raw)
-                            temp[key] = scheme
-                            targets.set(scheme.id(), scheme)
-                        }
+                if (!process.env.BASH_CSL_ADDR) throw new Error("no env of BASH_CSL_ADDR was found.")
+                let strs = process.env.BASH_CSL_ADDR.split("@")
+                if (strs.length < 2) throw new Error("invalid BASH_CSL_ADDR field: " + process.env.BASH_CSL_ADDR)
+                let addr = strs[0]
+                let ns = strs[1]
+                let plat = Helper.GetPlat()
+                let arch = Helper.GetArch()
+                let root = Const.WORKSAPCE
+                let settings = require(path.join(root, ".vscode/settings.json"))
+                let rawList = settings["ecode-go.targetList"]
+                let targets = new Map<string, Scheme>()
+                for (let name in rawList) {
+                    let otarget = rawList[name]
+                    let temp = {}
+                    for (let key in otarget) {
+                        let raw = otarget[key]
+                        let base = temp[raw.base]
+                        let scheme = new Scheme(name, key, base, raw)
+                        temp[key] = scheme
+                        targets.set(scheme.id(), scheme)
                     }
+                }
 
-                    for (let scheme of targets.values()) {
-                        let id = scheme.name + ".release" + "." + plat + "." + arch
-                        if ((svrs == null || svrs.length == 0 || svrs.indexOf(scheme.name) >= 0) && scheme.id() == id) {
-                            let exename = scheme.os == "windows" ? scheme.name + ".exe" : scheme.name
-                            let exepath: string = ""
-                            if (scheme.build_path) {
-                                exepath = path.isAbsolute(scheme.build_path) ?
-                                    path.join(scheme.build_path, scheme.name) :
-                                    path.join(root, scheme.build_path, scheme.name)
-                            } else {
-                                exepath = path.join(root, "exec", scheme.name)
+                for (let scheme of targets.values()) {
+                    let id = scheme.name + ".release" + "." + plat + "." + arch
+                    if ((svrs == null || svrs.length == 0 || svrs.indexOf(scheme.name) >= 0) && scheme.id() == id) {
+                        let exename = scheme.os == "windows" ? scheme.name + ".exe" : scheme.name
+                        let exepath: string = ""
+                        if (scheme.build_path) {
+                            exepath = path.isAbsolute(scheme.build_path) ?
+                                path.join(scheme.build_path, scheme.name) :
+                                path.join(root, scheme.build_path, scheme.name)
+                        } else {
+                            exepath = path.join(root, "exec", scheme.name)
+                        }
+                        let exefile = path.join(exepath, exename)
+                        try {
+                            let cmd = ""
+                            let opt = Helper.ExecOpt(exepath) as child_process.SpawnOptions
+                            opt.detached = true
+                            if (plat == "windows") {
+                                cmd = exefile
+                            } else if (plat == "darwin") {
+                                child_process.execSync(Helper.Format("chmod -R 777 {0}", exepath))
+                                cmd = Helper.Format("echo \"cd {0}\n{1}\" > /tmp/{2}; chmod 777 /tmp/{3}; open -a Terminal /tmp/{4}", exepath, exefile, exename, exename, exename)
+                                Helper.Log("[Start]Tips: go to [Terminal/Preferences/Profile/Shell] and set auto close when terminal is finished.")
+                            } else if (plat == "linux") {
+                                child_process.execSync(Helper.Format("chmod -R 777 {0}", exepath))
+                                cmd = exefile
                             }
-                            let exefile = path.join(exepath, exename)
-                            try {
-                                let cmd = ""
-                                let opt = Helper.ExecOpt(exepath) as child_process.SpawnOptions
-                                opt.detached = true
-                                if (plat == "windows") {
-                                    cmd = exefile
-                                } else if (plat == "darwin") {
-                                    child_process.execSync(Helper.Format("chmod -R 777 {0}", exepath))
-                                    cmd = Helper.Format("echo \"cd {0}\n{1}\" > /tmp/{2}; chmod 777 /tmp/{3}; open -a Terminal /tmp/{4}", exepath, exefile, exename, exename, exename)
-                                    Helper.Log("[Start]Tips: go to [Terminal/Preferences/Profile/Shell] and set auto close when terminal is finished.")
-                                } else if (plat == "linux") {
-                                    child_process.execSync(Helper.Format("chmod -R 777 {0}", exepath))
-                                    cmd = exefile
+                            if (scheme.start_args && scheme.start_args.length > 0) {
+                                for (let i = 0; i < scheme.start_args.length; i++) {
+                                    cmd += " " + scheme.start_args[i]
                                 }
-                                if (scheme.start_args && scheme.start_args.length > 0) {
-                                    for (let i = 0; i < scheme.start_args.length; i++) {
-                                        cmd += " " + scheme.start_args[i]
-                                    }
-                                }
-                                Helper.Log("[Start]{0}", cmd)
-                                child_process.spawn(cmd, opt)
-                            } catch (error) {
-                                Helper.LogError("[Start]run {0} err: {1}", exefile, error)
-                                reject(error)
                             }
+                            Helper.Log("[Start]{0}", cmd)
+                            child_process.spawn(cmd, opt)
+                        } catch (error) {
+                            Helper.LogError("[Start]run {0} err: {1}", exefile, error)
+                            reject(error)
                         }
                     }
-                    await new Promise((resolve) => setTimeout(resolve, 6000))
-                    resolve("[Server/Monitor]http://" + lcfg.csl.addr + "/ui/dc1/services/" + lcfg.csl.ns)
                 }
-            } catch (err) { reject(err) }
+                resolve("[Server/Monitor]" + addr + "/ui/dc1/services/" + ns)
+            } catch (err) {
+                Helper.LogError("[Start]err: {0}", err)
+                reject(err)
+            }
         })
     }
 }
--- .jenkins/libs/Stop.ts
+++ .jenkins/libs/Stop.ts
@@ -8,57 +8,51 @@
 //                   SEE LICENSE.md FOR MORE DETAILS.                    //
 //-----------------------------------------------------------------------//
 
-import * as path from "path"
 import * as http from "http"
 import { Helper } from "./Helper"
-import { Const } from "./Const"
 
 export class Stop {
     public static Process(svrs?: string[]): Promise<void> {
         return new Promise<void>(async (resolve, reject) => {
             try {
-                let lcfg: any
-                let sf = path.join(Const.WORKSAPCE, "exec/public/conn/conf/server.json")
-                if (Helper.HasFile(sf)) lcfg = require(sf)
-                else {
-                    sf = path.join(Const.WORKSAPCE, "exec/public/cgi/conf/server.json")
-                    if (Helper.HasFile(sf)) lcfg = require(sf)
+                if (!process.env.BASH_CSL_ADDR) throw new Error("no env of BASH_CSL_ADDR was found.")
+                let strs = process.env.BASH_CSL_ADDR.split("@")
+                if (strs.length < 2) throw new Error("invalid BASH_CSL_ADDR field: " + process.env.BASH_CSL_ADDR)
+                let addr = strs[0]
+                let ns = strs[1]
+                let caddr = addr + "/v1/health/checks/" + ns
+                let services = await new Promise<any[]>((resolve, reject) => {
+                    http.get(caddr, (resp) => {
+                        let ctt = ""
+                        resp.on("data", (chunk) => ctt += chunk)
+                        resp.on("end", () => resolve(JSON.parse(ctt)))
+                    }).on("error", (err) => reject(err))
+                })
+                let wait = false
+                for (let i = 0; i < services.length; i++) {
+                    let sid = services[i]["ServiceID"]
+                    let name = sid.split("@")[0]
+                    let ctrl = services[i]["Notes"]
+                    if (svrs == null || svrs.length == 0 || svrs.indexOf(name) >= 0) {
+                        await new Promise<void>((resolve) => {
+                            http.get("http://" + ctrl + "/close", (resp) => {
+                                if (!wait) wait = resp.statusCode == 200
+                                let ctt = ""
+                                resp.on("data", (chunk) => ctt += chunk)
+                                resp.on("end", () => { Helper.Log("[Stop]kill {0} resp: {1}", sid, ctt); resolve() })
+                            }).on("error", (err) => { Helper.LogError("[Stop]kill {0} failed: {1}", sid, err); resolve() })
+                        })
+                    }
                 }
-                if (!lcfg) {
-                    let err = "no default config(conn/cgi) was found."
-                    Helper.LogError("[Stop]{0}", err)
-                } else {
-                    let caddr = "http://" + lcfg.csl.addr + "/v1/health/checks/" + lcfg.csl.ns
-                    let services = await new Promise<any[]>((resolve, reject) => {
-                        http.get(caddr, (resp) => {
-                            let ctt = ""
-                            resp.on("data", (chunk) => ctt += chunk)
-                            resp.on("end", () => resolve(JSON.parse(ctt)))
-                        }).on("error", (err) => reject(err))
-                    })
-                    let wait = false
-                    for (let i = 0; i < services.length; i++) {
-                        let sid = services[i]["ServiceID"]
-                        let name = sid.split("@")[0]
-                        let ctrl = services[i]["Notes"]
-                        if (svrs == null || svrs.length == 0 || svrs.indexOf(name) >= 0) {
-                            await new Promise<void>((resolve) => {
-                                http.get("http://" + ctrl + "/close", (resp) => {
-                                    if (!wait) wait = resp.statusCode == 200
-                                    let ctt = ""
-                                    resp.on("data", (chunk) => ctt += chunk)
-                                    resp.on("end", () => { Helper.Log("[Stop]kill {0} resp: {1}", sid, ctt); resolve() })
-                                }).on("error", (err) => { Helper.LogError("[Stop]kill {0} failed: {1}", sid, err); resolve() })
-                            })
-                        }
-                    }
-                    if (wait) {
-                        Helper.Log("[Stop]waiting for all services been closed.")
-                        await new Promise((resolve) => setTimeout(resolve, 6000))
-                    }
+                if (wait) {
+                    Helper.Log("[Stop]waiting for all services been closed.")
+                    await new Promise((resolve) => setTimeout(resolve, 6000))
                 }
                 resolve()
-            } catch (err) { reject(err) }
+            } catch (err) {
+                Helper.LogError("[Stop]err: {0}", err)
+                reject(err)
+            }
         })
     }
 }
